03098e3188550af9c8e9bc74e01c02b06f00eb5b
ChrystianDuarte <chrystianduarte88@gmail.com>
2016-05-10 00:10:43 -0300

    EJB Client

---
+++ MoveMeWEB/src/com/cl/moveme/controlers/MovemeTestEJB.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cl.moveme.controlers;
+
+/**
+ * @author Chrystian Duarte
+ *
+ */
+public class MovemeTestEJB {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
